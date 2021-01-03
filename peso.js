3e04db6077e872da0e8e05c2c9da1dd342151088
Renegado87 <rcmaster87@gmail.com>
2021-01-03 12:28:44 -0600

    master

---
+++ peso.js
@@ -0,0 +1,12 @@
+
+
+var usuario = prompt("Cual es tu peso?");
+var peso = parseInt (usuario);
+var g_tierra = 9.8;
+var g_marte = 3.7;
+var g_jupiter = 24.8;
+var peso_final;
+peso_final = peso * g_marte / g_tierra;
+peso_final = parseInt (peso_final);
+document.write("Tu peso en marte es <strong>" + peso_final + " Kilos</strong>");
+ 
